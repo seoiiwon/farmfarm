7be2bb29df2c6069a471947a3659bcc079f32dfd
Seo Jiwon <wldnjstj99@gmail.com>
2024-11-25 18:00:39 +0900

    status api update

--- api/statusData.py
+++ api/statusData.py
@@ -21,22 +21,32 @@ class EnvStatusSchema(BaseModel):
     waterTemperature: float
     recorded_at: datetime
 
+
+
 @router.post("/get_env/{_crop_id}", status_code=status.HTTP_200_OK)
-async def getNewEnvStatus(EnvStatusSchema: EnvStatusSchema, _crop_id: int, db: Session=Depends(get_db)):
-    # crop_id가 존재하는지 확인
+async def getNewEnvStatus(EnvStatusSchema: EnvStatusSchema, _crop_id: int, db: Session = Depends(get_db)):
     crop = db.query(CropModel).filter(CropModel.id == _crop_id).first()
     if not crop:
-        # crop_id가 유효하지 않으면 404 에러 발생
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Crop not found")
+
+    last_env_status = db.query(EnvironmentStatusModel).filter(EnvironmentStatusModel.crop_id == _crop_id)\
+        .order_by(EnvironmentStatusModel.recorded_at.desc()).first()
+
+    temperature = EnvStatusSchema.temperature if EnvStatusSchema.temperature is not None else (last_env_status.temperature if last_env_status else 0)
+    humidity = EnvStatusSchema.humidity if EnvStatusSchema.humidity is not None else (last_env_status.humidity if last_env_status else 0)
+    solid_humidity = EnvStatusSchema.solidHumidity if EnvStatusSchema.solidHumidity is not None else (last_env_status.solidHumidity if last_env_status else 0)
+    illuminance = EnvStatusSchema.illuminance if EnvStatusSchema.illuminance is not None else (last_env_status.illuminance if last_env_status else 0)
+    co2_concentration = EnvStatusSchema.co2Concentration if EnvStatusSchema.co2Concentration is not None else (last_env_status.co2Concentration if last_env_status else 0)
+    water_temperature = EnvStatusSchema.waterTemperature if EnvStatusSchema.waterTemperature is not None else (last_env_status.waterTemperature if last_env_status else 0)
 
     try:
         newEnv = EnvironmentStatusModel(
-            temperature=EnvStatusSchema.temperature,
-            humidity=EnvStatusSchema.humidity,
-            solidHumidity=EnvStatusSchema.solidHumidity,
-            illuminance=EnvStatusSchema.illuminance,
-            co2Concentration=EnvStatusSchema.co2Concentration,
-            waterTemperature=EnvStatusSchema.waterTemperature,
+            temperature=temperature,
+            humidity=humidity,
+            solidHumidity=solid_humidity,
+            illuminance=illuminance,
+            co2Concentration=co2_concentration,
+            waterTemperature=water_temperature,
             recorded_at=datetime.now(),
             crop_id=_crop_id
         )
@@ -47,7 +57,7 @@ async def getNewEnvStatus(EnvStatusSchema: EnvStatusSchema, _crop_id: int, db: S
         db.rollback()
         print(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+
     return JSONResponse(
         content={
             "id": newEnv.id,
